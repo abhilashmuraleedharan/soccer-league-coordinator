29ed590303cc9849f7363d10cacfdd318d871fad
Abhilash Muraleedharan <amuraleedharan13@gmail.com>
2018-06-23 08:29:25 +0530

    Adds player details

--- SoccerLeagueCoordinator.playground/Contents.swift
+++ SoccerLeagueCoordinator.playground/Contents.swift
@@ -1,1 +1,102 @@
+/* The objective of this project is to divide 18 children who have signed up for the league into three even teams
+ * 1. Dragons
+ * 2. Sharks
+ * 3. Raptors
+ */
 
+/* Part 1
+ * a) Store the information for each player
+ * b) Create an empty collection constant to hold all the players’ data
+ * c) Enter the player data
+ */
+// Use an array of dictionary items to hold each player's information
+let player1 =  [["name":"Joe Smith"],
+                ["height":42],
+                ["experience":true],
+                ["guardian":"Jim and Jan Smith"]
+]
+let player2 =  [["name":"Jill Tanner"],
+                ["height":36],
+                ["experience":true],
+                ["guardian":"Clara Tanner"]
+]
+let player3 =  [["name":"Bill Bon"],
+                ["height":43],
+                ["experience":true],
+                ["guardian":"Sara and Jenny Bon"]
+]
+let player4 =  [["name":"Eva Gordon"],
+                ["height":45],
+                ["experience":false],
+                ["guardian":"Wendy and Mike Gordon"]
+]
+let player5 =  [["name":"Matt Gill"],
+                ["height":40],
+                ["experience":false],
+                ["guardian":"Charles and Sylvia Gill"]
+]
+let player6 =  [["name":"Kimmy Stein"],
+                ["height":41],
+                ["experience":false],
+                ["guardian":"Bill and Hillary Stein"]
+]
+let player7 =  [["name":"Sammy Adams"],
+                ["height":45],
+                ["experience":false],
+                ["guardian":"Jeff Adams"]
+]
+let player8 =  [["name":"Karl Saygan"],
+                ["height":42],
+                ["experience":true],
+                ["guardian":"Heather Bledsoe"]
+]
+let player9 =  [["name":"Suzane Greenberg"],
+                ["height":44],
+                ["experience":true],
+                ["guardian":"Henrietta Dumas"]
+]
+let player10 = [["name":"Sal Dali"],
+                ["height":41],
+                ["experience":false],
+                ["guardian":"Gala Dali"]
+]
+let player11 = [["name":"Joe Kavalier"],
+                ["height":39],
+                ["experience":false],
+                ["guardian":"Sam and Elaine Kavalier"]
+]
+let player12 = [["name":"Ben Finkelstein"],
+                ["height":44],
+                ["experience":false],
+                ["guardian":"Aaron and Jill Finkelstein"]
+]
+let player13 = [["name":"Diego Soto"],
+                ["height":41],
+                ["experience":true],
+                ["guardian":"Robin and Sarika Soto"]
+]
+let player14 = [["name":"Chloe Alaska"],
+                ["height":47],
+                ["experience":false],
+                ["guardian":"David and Jamie Alaska"]
+]
+let player15 = [["name":"Arnold Willis"],
+                ["height":43],
+                ["experience":false],
+                ["guardian":"Claire Willis"]
+]
+let player16 = [["name":"Phillip Helm"],
+                ["height":44],
+                ["experience":true],
+                ["guardian":"Thomas Helm and Eva Jones"]
+]
+let player17 = [["name":"Les Clay"],
+                ["height":42],
+                ["experience":true],
+                ["guardian":"Wynonna Brown"]
+]
+let player18 = [["name":"Herschel Krustofski"],
+                ["height":45],
+                ["experience":true],
+                ["guardian":"Hyman and Rachel Krustofski"]
+]
