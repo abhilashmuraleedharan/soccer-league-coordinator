a7166b2998dd143bc000153a7502fb98af21acdc
Abhilash Muraleedharan <amuraleedharan13@gmail.com>
2018-06-24 10:14:47 +0530

    Corrects players datatype to adhere to project guidelines

--- SoccerLeagueCoordinator.playground/Contents.swift
+++ SoccerLeagueCoordinator.playground/Contents.swift
@@ -12,99 +12,100 @@
  * b) Display all the players' information
  */
 
-// Use an array of dictionary [[String: Any]] to hold all player's information.
-let players = [
-               ["name":"Joe Smith",
-                "height":42.0,
-                "experience":true,
-                "guardian":"Jim and Jan Smith"
-               ],
-               ["name":"Jill Tanner",
-                "height":36.0,
-                "experience":true,
-                "guardian":"Clara Tanner"
-               ],
-               ["name":"Bill Bon",
-                "height":43.0,
-                "experience":true,
-                "guardian":"Sara and Jenny Bon"
-               ],
-               ["name":"Eva Gordon",
-                "height":45.0,
-                "experience":false,
-                "guardian":"Wendy and Mike Gordon"
-               ],
-               ["name":"Matt Gill",
-                "height":40.0,
-                "experience":false,
-                "guardian":"Charles and Sylvia Gill"
-               ],
-               ["name":"Kimmy Stein",
-                "height":41.0,
-                "experience":false,
-                "guardian":"Bill and Hillary Stein"
-               ],
-               ["name":"Sammy Adams",
-                "height":45.0,
-                "experience":false,
-                "guardian":"Jeff Adams"
-               ],
-               ["name":"Karl Saygan",
-                "height":42.0,
-                "experience":true,
-                "guardian":"Heather Bledsoe"
-               ],
-               ["name":"Suzane Greenberg",
-                "height":44.0,
-                "experience":true,
-                "guardian":"Henrietta Dumas"
-               ],
-               ["name":"Sal Dali",
-                "height":41.0,
-                "experience":false,
-                "guardian":"Gala Dali"
-               ],
-               ["name":"Joe Kavalier",
-                "height":39.0,
-                "experience":false,
-                "guardian":"Sam and Elaine Kavalier"
-               ],
-               ["name":"Ben Finkelstein",
-                "height":44.0,
-                "experience":false,
-                "guardian":"Aaron and Jill Finkelstein"
-               ],
-               ["name":"Diego Soto",
-                "height":41.0,
-                "experience":true,
-                "guardian":"Robin and Sarika Soto"
-               ],
-               ["name":"Chloe Alaska",
-                "height":47.0,
-                "experience":false,
-                "guardian":"David and Jamie Alaska"
-               ],
-               ["name":"Arnold Willis",
-                "height":43.0,
-                "experience":false,
-                "guardian":"Claire Willis"
-               ],
-               ["name":"Phillip Helm",
-                "height":44.0,
-                "experience":true,
-                "guardian":"Thomas Helm and Eva Jones"
-               ],
-               ["name":"Les Clay",
-                "height":42.0,
-                "experience":true,
-                "guardian":"Wynonna Brown"
-               ],
-               ["name":"Herschel Krustofski",
-                "height":45.0,
-                "experience":true,
-                "guardian":"Hyman and Rachel Krustofski"
-               ]
-              ]
+// Use a dictionary [String:Any] to hold all details of a player.
+let player1:[String: Any] = ["name":"Joe Smith",
+                             "height":42.0,
+                             "experience":true,
+                             "guardian":"Jim and Jan Smith"
+                            ]
+let player2:[String: Any] = ["name":"Jill Tanner",
+                             "height":36.0,
+                             "experience":true,
+                             "guardian":"Clara Tanner"
+                            ]
+let player3:[String: Any] = ["name":"Bill Bon",
+                             "height":43.0,
+                             "experience":true,
+                             "guardian":"Sara and Jenny Bon"
+                            ]
+let player4:[String: Any] = ["name":"Eva Gordon",
+                             "height":45.0,
+                             "experience":false,
+                             "guardian":"Wendy and Mike Gordon"
+                            ]
+let player5:[String: Any] = ["name":"Matt Gill",
+                             "height":40.0,
+                             "experience":false,
+                             "guardian":"Charles and Sylvia Gill"
+                            ]
+let player6:[String: Any] = ["name":"Kimmy Stein",
+                             "height":41.0,
+                             "experience":false,
+                             "guardian":"Bill and Hillary Stein"
+                            ]
+let player7:[String: Any] = ["name":"Sammy Adams",
+                             "height":45.0,
+                             "experience":false,
+                             "guardian":"Jeff Adams"
+                            ]
+let player8:[String: Any] = ["name":"Karl Saygan",
+                             "height":42.0,
+                             "experience":true,
+                             "guardian":"Heather Bledsoe"
+                            ]
+let player9:[String: Any] = ["name":"Suzane Greenberg",
+                             "height":44.0,
+                             "experience":true,
+                             "guardian":"Henrietta Dumas"
+                            ]
+let player10:[String: Any] = ["name":"Sal Dali",
+                              "height":41.0,
+                              "experience":false,
+                              "guardian":"Gala Dali"
+                             ]
+let player11:[String: Any] = ["name":"Joe Kavalier",
+                              "height":39.0,
+                              "experience":false,
+                              "guardian":"Sam and Elaine Kavalier"
+                             ]
+let player12:[String: Any] = ["name":"Ben Finkelstein",
+                              "height":44.0,
+                              "experience":false,
+                              "guardian":"Aaron and Jill Finkelstein"
+                             ]
+let player13:[String: Any] = ["name":"Diego Soto",
+                              "height":41.0,
+                              "experience":true,
+                              "guardian":"Robin and Sarika Soto"
+                             ]
+let player14:[String: Any] = ["name":"Chloe Alaska",
+                              "height":47.0,
+                              "experience":false,
+                              "guardian":"David and Jamie Alaska"
+                             ]
+let player15:[String: Any] = ["name":"Arnold Willis",
+                              "height":43.0,
+                              "experience":false,
+                              "guardian":"Claire Willis"
+                             ]
+let player16:[String: Any] = ["name":"Phillip Helm",
+                              "height":44.0,
+                              "experience":true,
+                              "guardian":"Thomas Helm and Eva Jones"
+                             ]
+let player17:[String: Any] = ["name":"Les Clay",
+                              "height":42.0,
+                              "experience":true,
+                              "guardian":"Wynonna Brown"
+                             ]
+let player18:[String: Any] = ["name":"Herschel Krustofski",
+                              "height":45.0,
+                              "experience":true,
+                              "guardian":"Hyman and Rachel Krustofski"
+                             ]
+
+// Use a collection constant to hold all the players’ data
+let players = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
 
 // Function to print to console player details in a collection
 func displayPlayerDetails(inCollection list: [[String: Any]]) {
@@ -234,7 +235,7 @@ func sendToGuardians(personalLetters letters: [String]) {
     }
 }
 
-// Prepare and store letters to be sent to each Team's player's guardians
+// Prepare and store letters to be sent out to each Team's player's guardians
 teamDragonsLetters = formatLetterToGuardiansOfPlayers(aboutPracticeDateAndTime: dragonsPracticeTime, ofTeam: teamDragons, havingName: "Dragons")
 teamSharksLetters = formatLetterToGuardiansOfPlayers(aboutPracticeDateAndTime: sharksPraciceTime, ofTeam: teamSharks, havingName: "Sharks")
 teamRaptorsLetters = formatLetterToGuardiansOfPlayers(aboutPracticeDateAndTime: raptorsPracticeTime, ofTeam: teamRaptors, havingName: "Raptors")
@@ -265,5 +266,3 @@ print("-------------------------------------------------------------------------
 print("Average height of players of Team Dragons is \(getPlayersAvgHeight(ofTeam: teamDragons)) inches")
 print("Average height of players of Team Sharks is \(getPlayersAvgHeight(ofTeam: teamSharks)) inches")
 print("Average height of players of Team Raptors is \(getPlayersAvgHeight(ofTeam: teamRaptors)) inches")
-
-
