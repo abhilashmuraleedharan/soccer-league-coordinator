fa2d609afe8f3551fa6fecbe74b1abfccd700280
Abhilash Muraleedharan <amuraleedharan13@gmail.com>
2018-06-29 22:15:02 +0530

    Uses objects to model soccer coordinator project

--- ObjectOrientedSoccerLeagueCoordinator.playground/Contents.swift
+++ ObjectOrientedSoccerLeagueCoordinator.playground/Contents.swift
@@ -12,7 +12,7 @@
  *
  */
 
-// Player Class to model a league player
+// Class Player to model a league player
 class Player {
     let name: String
     let height: Double
@@ -56,7 +56,7 @@ struct Email {
     }
 }
 
-// Team Class to model a league team
+// Class Team to model a league team
 class Team {
     let name: String
     var players: [Player]
@@ -122,7 +122,7 @@ class Team {
     }
 }
 
-// League Class to model the Soccer League
+// Class League to model the Soccer League
 class League {
     let name: String
     let teamsCount: Int
@@ -162,7 +162,7 @@ class League {
     }
     
     /// Instance method to create teams
-    func create(teams: [String]) {
+    func sortPlayers(intoTeams: [String]) {
         
         if (teams.count != teamsCount) {
             print("Teams Count Mismatch. Pass the names of all teams")
@@ -239,9 +239,86 @@ class League {
     
 }
 
-// As a soccer league coordinator use the above objects to create your teams
-let league = League(name: "FIFA", withTotalTeams: 3, havingStrength: 6)
-let player1 = Player(name: "Joe Smith", height: 42.0, experience: true, guardian: "Jim and Jan Smith", guardianEmailId: "jimsmith@gmail.com")
-player1.information()
-league.add(player: player1)
+//
+//
+//
 
+/* 
+ * As a soccer league coordinator use the above defined custom objects to create your league and teams
+ */
+
+// Create a League 
+let league = League(name: "TreeHouse University Soccer League", withTotalTeams: 3, havingStrength: 6)
+
+// Create League Players
+let player1 = Player(name: "Joe Smith", height: 42.0, experience: true, guardian: "Jim and Jan Smith", guardianEmailId: "jimsmith@gmail.com")
+let player2 = Player(name: "Jill Tanner", height: 36.0, experience: true, guardian: "Clara Tanner", guardianEmailId: "clara.tanner@gmail.com")
+let player3 = Player(name: "Bill Bon", height: 43.0, experience: true, guardian: "Sara and Jenny Bon", guardianEmailId: "sara@gmail.com")
+let player4 = Player(name: "Eva Gordon", height: 45.0, experience: false, guardian: "Wendy and Mike Gordon", guardianEmailId: "wendy@gmail.com")
+let player5 = Player(name: "Matt Gill", height: 40.0, experience: false, guardian: "Charles and Sylvia Gill", guardianEmailId: "charles@gmail.com")
+let player6 = Player(name: "Kimmy Stein", height: 41.0, experience: false, guardian: "Bill and Hillary Stein", guardianEmailId: "bill@gmail.com")
+let player7 = Player(name: "Sammy Adams", height: 45.0, experience: false, guardian: "Jeff Adams", guardianEmailId: "jeff.adams@gmail.com")
+let player8 = Player(name: "Karl Saygan", height: 42.0, experience: true, guardian: "Heather Bledsoe", guardianEmailId: "heather@gmail.com")
+let player9 = Player(name: "Suzane Greenberg", height: 44.0, experience: true, guardian: "Henrietta Dumas", guardianEmailId: "henrietta@gmail.com")
+let player10 = Player(name: "Sal Dali", height: 41.0, experience: false, guardian: "Gala Dali", guardianEmailId: "gala.dali@gmail.com")
+let player11 = Player(name: "Joe Kavalier", height: 39.0, experience: false, guardian: "Sam and Elaine Kavalier", guardianEmailId: "sam@gmail.com")
+let player12 = Player(name: "Ben Finkelstein", height: 44.0, experience: false, guardian: "Aaron and Jill Finkelstein", guardianEmailId: "aaron@gmail.com")
+let player13 = Player(name: "Diego Soto", height: 41.0, experience: true, guardian: "Robin and Sarika Soto", guardianEmailId: "robin@gmail.com")
+let player14 = Player(name: "Chloe Alaska", height: 47.0, experience: false, guardian: "David and Jamie Alaska", guardianEmailId: "david@gmail.com")
+let player15 = Player(name: "Arnold Willis", height: 43.0, experience: false, guardian: "Claire Willis", guardianEmailId: "claire@gmail.com")
+let player16 = Player(name: "Phillip Helm", height: 44.0, experience: true, guardian: "Thomas Helm and Eva Jones", guardianEmailId: "thomas@gmail.com")
+let player17 = Player(name: "Les Clay", height: 42.0, experience: true, guardian: "Wynonna Brown", guardianEmailId: "wynonna@gmail.com")
+let player18 = Player(name: "Herschel Krustofski", height: 45.0, experience: true, guardian: "Hyman and Rachel Krustofski", guardianEmailId: "hyman@gmail.com")
+
+// You can view a player information using the information instance method
+player1.information()
+player13.information()
+
+// Add the Players to the League
+let players = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
+for leaguePlayer in players {
+    league.add(player: leaguePlayer)
+}
+
+// Create the league Teams
+let leagueTeams = ["Dragons", "Sharks", "Raptors"] // Since while creating league object, teams count was specified as 3
+
+// Distribute players across teams
+league.sortPlayers(intoTeams: leagueTeams)
+
+// Storing team details in respective team variables
+let teamDragons = league.teams[0]
+let teamSharks = league.teams[1]
+let teamRaptors = league.teams[2]
+
+// View each team detail using the information method
+teamDragons.information()
+teamSharks.information()
+teamRaptors.information()
+
+// View average height of players of each team using the averageHeightOfPlayers() method
+print("Average height of players of Team Dragons = \(teamDragons.averageHeightOfPlayers()) inches")
+print("Average height of players of Team Sharks = \(teamSharks.averageHeightOfPlayers()) inches")
+print("Average height of players of Team Raptors = \(teamRaptors.averageHeightOfPlayers()) inches")
+
+// Give each team their practice date and time information
+let practiceTime = [
+                     "Team Dragons": "March 17, 1pm", 
+                     "Team Sharks": "March 17, 3pm", 
+                     "Team Raptors": "March 18, 1pm"
+                   ]
+
+league.assignToTeams(practiceDateTime: [ practiceTime["Team Dragons"], practiceTime["Team Sharks"], practiceTime["Team Raptors"] ])
+
+// Prepare and send emails to guardians of all league players 
+// informing them of their ward's team and practice time details
+league.informGuardians()
+
+// Consolidate all emails to a constant 
+let emails = league.teams[0].emails + league.teams[1].emails + league.teams[2].emails
+
+// Preview all emails
+emails.preview()
+
+// View the League's whole information
+league.information()
